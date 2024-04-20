4884f739ac27747fe0bfcd38351da81d778947ad
neelanshagr <neelanshagrawal16@gmail.com>
2024-04-21 00:40:04 +0530

    login and signup frontend

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
-
+import {dbConnection} from './database/dbConnection.js'
 
 const app = express();
 dotenv.config({path: "./config/config.env"})
 
 // to connect backend to frontend
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials:true
-}))
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials:true
+// }))
 
 
 //app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+ //app.use(express.json());
+ //app.use(express.urlencoded({extended:true}));
 
 
 
 //app.use('/api/v1/user', userRouter);
 
 
-///dbConnection();
+dbConnection();
 
 
 export default app;
---
+++ frontend/src/components/Login/login.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import eye icons
+import  './login.css'
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [inpval, setInpval] =  useState({
+    email:"",
+    password:"",
+  })
+
+  const setVal= (e)=>{
+    // console.log(e.target.value);
+    const {name,value} =e.target;
+    
+    setInpval(()=>{
+      return {
+        ...inpval,
+        [name]:value
+      }
+    })
+  };
+
+  const loginUser =  (e) =>{
+    e.preventDefault();
+
+    const {email, password} =inpval;
+
+    if(email === ""){
+      alert("please enter your email");
+    }
+    else if(!email.includes('@')){
+      alert('please enter a valid email address')
+    }
+    else if(password === ""){
+      alert("please enter your password");
+    }
+    else if(password.length<6){
+      alert("Password must contain at least 6 letters")
+    }
+    else{
+      console.log("user login successfully")
+    }
+  }
+
+
+  return (
+    <>
+      <section>
+        <div className="form_data">
+          <div className="form_heading">
+            <h1>Welcome Back, Log In</h1>
+            <p>Hi, we are glad you are back. Please login.</p>
+          </div>
+          <form>   
+
+            <div className="form_input">
+              <label htmlFor="email">Email</label>
+              <input type="email" value={inpval.email} onChange={setVal} name="email" id="email" placeholder="Enter your Email Address" />
+            </div>
+
+            <div className="form_input">
+              <label htmlFor="password">Password</label>
+              <div className="two">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  value={inpval.password}
+                  onChange={setVal}
+                  id="password"
+                  placeholder="Enter your password"
+                />
+                {/* Show/hide password button */}
+                <div
+                  className="showpass"
+                  onClick={() => setShowPassword(prevState => !prevState)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </div>
+              </div>
+            </div>
+
+            <button className="btn" onClick={loginUser}>Login</button>
+            <p>Dont have an account? - <a href="/register">SignUp</a></p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
